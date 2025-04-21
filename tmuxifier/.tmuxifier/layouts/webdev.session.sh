e26247cd5d3da484392967e06edced934689f929
Drvulcanlord <122398995+Eli-Watson@users.noreply.github.com>
2025-04-21 09:12:25 -0600

    Add Tmuxifier Layouts
    forgot to add my custom layouts (the whole point of tmuxifier) to the
    repo.

---
+++ tmuxifier/.tmuxifier/layouts/webdev.session.sh
@@ -0,0 +1,24 @@
+# Set a custom session root path. Default is `$HOME`.
+# Must be called before `initialize_session`.
+session_root "~/Gitub/"
+
+# Create session with specified name if it does not already exist. If no
+# argument is given, session name will be based on layout file name.
+if initialize_session "webdev"; then
+
+  # Create a new window inline within session layout definition.
+    new_window "bash"
+    new_window "nvim"
+    new_window "server"
+    
+    select_window "nvim"
+    run_cmd "nvim"
+
+    select_window "server"
+    run_cmd "cd ~/Github/"
+    run_cmd "npx live-server"
+    
+fi
+
+# Finalize session creation and switch/attach to it.
+finalize_and_go_to_session
